6da8652b958058b7b4408d4844dd0e0f92562d2b
MjImani <mjimani.ir@gmail.com>
2021-11-16 23:04:27 +0330

    before merge

---
+++ src/main/java/ir/mjimani/demo/Result.java
@@ -0,0 +1,16 @@
+package ir.mjimani.demo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Result {
+	
+	private String name;
+	private String percentage;
+	private String result;
+}
---
+++ src/main/java/ir/mjimani/demo/Student.java
@@ -0,0 +1,15 @@
+package ir.mjimani.demo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+	private String registrationNumber;
+	private String name;
+	private String grade;
+}
